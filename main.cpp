f037bedf10ba93191b712d42850fc0f422721126
arxanev <arxanev@gmail.com>
2025-04-11 21:25:41 +0600

    Use comparison

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "include/Loader.h"
 #include "include/FileWriter.h"
+#include "include/Comparator.h"
 
 void PrintUsage(const char* program_name) {
     std::cerr << "Usage: " << program_name << " <branch1> <branch2>\n"
@@ -19,4 +20,13 @@ int main(int argc, char* argv[]) {
     const std::string output_filename = "result.json";
     auto lhs = CreateLoader(branch1);
     auto rhs = CreateLoader(branch2);
+
+    Comparator comparator;
+    puts("loading");
+    auto comparison = comparator.Compare(branch1, lhs->Load(), branch2, rhs->Load());
+    std::stringstream ss;
+    comparison->stringify(ss, 2);
+    puts("recording");
+
+    FileWriter::Save(output_filename, ss.str());
 }
